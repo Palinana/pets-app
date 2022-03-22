eb18158752f23dae4acd8f6de9e1a311d59bf85e
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2022-03-21 20:02:57 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+          <div>
+             Hello
+          </div>      
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
